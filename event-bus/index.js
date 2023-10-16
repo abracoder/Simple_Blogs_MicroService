7aea2ae35feafac8ad055c813928c478a0439aa6
abracoder <abrakilvis@gmail.com>
2023-10-16 07:19:33 +0530

    fail safe mechanism added even if query Service  is down

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,15 +7,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
+
 app.post('/events',(req,res) => {
     const event = req.body;
+    events.push(event);
     axios.post('http://localhost:4000/events',event);
     axios.post('http://localhost:4001/events',event);
     axios.post('http://localhost:4002/events',event);
     axios.post('http://localhost:4003/events',event);
     
+    console.log("events sent");
     res.send({status : 'OK'});
+    
 });
+app.get('/events',(req,res) => {
+    res.send(events);
+})
 
 app.listen(4005,()=>{
     console.log("listening on 4005");
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
@@ -8,17 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-
-
-app.get("/posts",(req,res)=>{
-
-    res.send(posts);
-
-});
-
-app.post("/events",(req,res)=>{
-    const { type, data } = req.body;
-
+const handleEvent = (type,data) =>{
     if(type === 'PostCreated'){
         const { id, title } = data;
         posts[id] = {id,title,comments:[]};
@@ -42,6 +33,19 @@ app.post("/events",(req,res)=>{
         console.log("query comment Updated",data);
 
     }
+}
+
+
+app.get("/posts",(req,res)=>{
+
+    res.send(posts);
+
+});
+
+app.post("/events",(req,res)=>{
+    const { type, data } = req.body;
+    handleEvent(type,data);
+
     console.log("query Post",posts);
 
     res.send({});
@@ -49,6 +53,12 @@ app.post("/events",(req,res)=>{
 });
 
 
-app.listen(4002,()=> {
+app.listen(4002,async()=> {
     console.log("listening on 4002");
+    const res = await axios.get('http://localhost:4005/events');
+    for( let event of res.data){
+        console.log('Processing event: ', event.type);
+        handleEvent(event.type,event.data);
+
+    }
 });
