5c2f776855a2719d22cff5ced75185a55d7a33b5
abracoder <abrakilvis@gmail.com>
2023-10-16 05:37:05 +0530

    comment status added as pending and created a moderations service

---
+++ moderation/index.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const axios = require ('axios');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/events',(req,res)=>{
+
+});
+
+app.listen(4003,()=>{
+    console.log("listening on 4003");
+});
